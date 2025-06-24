7b4328a296246faeaf9a2795380c140747ebca17
root <root@Sneha.localdomain>
2025-06-24 10:30:27 +0530

    Output files created and executed the program

--- datahubproject/items.py
+++ datahubproject/items.py
@@ -1,4 +1,4 @@
-# Define here the models for your scraped items
+ # Define here the models for your scraped items
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
@@ -9,16 +9,16 @@ import scrapy
 class DatahubprojectItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    breadcrumbs=scrapy.Field()
+   # breadcrumbs=scrapy.Field()
     primary_image_url=scrapy.Field()
     brand=scrapy.Field()
     product_name=scrapy.Field()
     price=scrapy.Field()
     reviews=scrapy.Field()
-    color=scrapy.Field()
-    sizes=scrapy.Field()
-    description=scrapy.Field()
-    sku=scrapy.Field()
-    product_id=scrapy.Field()
-    product_url=scrapy.Field()
-    image_urls=scrapy.Field()
+    colour=scrapy.Field()
+   # sizes=scrapy.Field()
+   # description=scrapy.Field()
+   # sku=scrapy.Field()
+   # product_id=scrapy.Field()
+   # product_url=scrapy.Field()
+   # image_urls=scrapy.Field()
--- datahubproject/spiders/carbonspider.py
+++ datahubproject/spiders/carbonspider.py
@@ -1,38 +1,34 @@
 import scrapy
+import json
 from datahubproject.items import DatahubprojectItem
 
 
 class CarbonspiderSpider(scrapy.Spider):
     name = "carbonspider"
     allowed_domains = ["carbon38.com"]
-    start_urls = ["https://www.carbon38.com/product/tessa-top-primary-stripe"]
+    start_urls = ["https://www.carbon38.com/shop-all-activewear/tops"]
 
     def parse(self, response):
          # Crawl all products on the current page
-        products = response.css('div.ProductItem').getall()
-        for product in products:
-            yield response.follow(product, callback=self.parse_product)
+        product_links = response.css('div.ProductItem__Info h2.ProductItem__Title a::attr(href)').getall()
+        for link in product_links:
+            full_link = response.urljoin(link)
+            yield scrapy.Request(full_link, callback=self.parse_product)
 
-        # Go to next page (pagination)
-        next_page = response.css('a.action.next::attr(href)').get()
-        if next_page:
-            yield response.follow(next_page, callback=self.parse)
 
     def parse_product(self, response):
         item = DatahubprojectItem()
-        item['breadcrumbs'] =
-        item['primary_image_url'] = response.css('img.ProductItem__Image::attr(src)').get()
-        item['brand'] = response.css('h3.ProductItem__Designer::text').get()
-        item['product_name'] = response.css('h2.ProductItem__Title.Heading a::text').get()
-        item['price'] = response.css('span.ProductItem__Price.Price::text').get()
-        item['reviews'] = len(response.css('div.yotpo-main-layout.yotpo-main-reviews-widget .yotpo-review'))
+       # item['breadcrumbs'] =
+        item['primary_image_url'] = response.css('img.fotorama__img::attr(src)').get()
+        item['brand'] = response.css('h2.ProductMeta__Vendor.Heading.u-h1::text').get()
+        item['product_name'] = response.css('h1.ProductMeta__Title.Heading::text').get()
+        item['price'] = response.css('span.ProductMeta__Price.Price::text').get()
+        item['reviews'] = len(response.css('div.yotpo-review'))
         item['colour'] = response.css('input.ColorSwatch__Radio::attr(value)').get()
-        item['sizes'] =
-        item['description'] =
-        item['sku'] =
-        item['product_id'] =
-        item['product_url'] =
-        item['image_urls'] =
+       # item['sizes'] =
+       # item['description'] =
+       # item['sku'] =
+       # item['product_id'] =
+       # item['product_url'] =
+       # item['image_urls'] =
         yield item
-        pass
-
